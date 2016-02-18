fd4fdfd63cc61ed7e0335fe87fc2246525f659b0
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-18 09:16:54 -0700

    addButton works

---
+++ app.js
@@ -0,0 +1,17 @@
+$('#addButton').on('click', function(){
+
+  for(var i = 0;i <= 11;i++){
+    $('#containingDiv').append('<p>what what what</p>');
+
+  }
+
+});
+
+$('#removeButton').on('click', function(){
+
+  for(var i = 0;i <= 11;i++){
+    $('#containingDiv').append('<p>what what what</p>');
+
+  }
+
+});
